dd9e01958600013f9fe23e4658a7403e01b79577
popeye0618 <popeye0618@gmail.com>
2026-08-03 21:22:36 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/com/harucut/HarucutApplication.java
@@ -0,0 +1,13 @@
+package com.harucut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarucutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HarucutApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/harucut/HarucutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harucut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarucutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
