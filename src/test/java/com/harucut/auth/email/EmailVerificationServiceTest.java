34e40587fa047bbfa19bea0c073be06d4a07d478
popeye0618 <popeye0618@gmail.com>
2026-08-13 16:14:56 +0900

    test: 테스트 환경 프로필 추가 및 메일 설정 적용
    - HarucutApplicationTests에 @ActiveProfiles("test") 추가
    - application-test.yaml에 Mailpit 테스트 메일 환경 설정 추가
    - EmailVerificationServiceTest에서 불필요한 BDDMockito import 제거

--- src/test/java/com/harucut/auth/email/EmailVerificationServiceTest.java
+++ src/test/java/com/harucut/auth/email/EmailVerificationServiceTest.java
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.BDDMockito;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.redis.core.StringRedisTemplate;
